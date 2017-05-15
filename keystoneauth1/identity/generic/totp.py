3841678a5980e280e46468b8b9bbe4c1fc659d0b
chuongntv <chuongntv.1409@gmail.com>
2017-05-15 17:41:34 +0700

    add logger

--- keystoneauth1/identity/generic/totp.py
+++ keystoneauth1/identity/generic/totp.py
@@ -12,8 +12,8 @@
 
 from positional import positional
 
-from keystoneauth1.identity.generic import base
 from keystoneauth1.identity import v3
+from keystoneauth1.identity.generic import base
 
 
 class Totp(base.BaseGenericPlugin):
@@ -28,25 +28,26 @@ class Totp(base.BaseGenericPlugin):
     """
 
     @positional()
-    def __init__(self, auth_url, username=None, user_id=None, passcode=None,
+    def __init__(self, auth_url, username=None, user_id=None, password=None, passcode=None,
                  user_domain_id=None, user_domain_name=None, **kwargs):
         super(Totp, self).__init__(auth_url=auth_url, **kwargs)
 
         self._username = username
         self._user_id = user_id
         self._passcode = passcode
+        self._password = password
         self._user_domain_id = user_domain_id
         self._user_domain_name = user_domain_name
 
     def create_plugin(self, session, version, url, raw_status=None):
-
         u_domain_id = self._user_domain_id or self._default_domain_id
         u_domain_name = self._user_domain_name or self._default_domain_name
 
         return v3.TOTP(auth_url=url,
-                           user_id=self._user_id,
-                           username=self._username,
-                           user_domain_id=u_domain_id,
-                           user_domain_name=u_domain_name,
-                           passcode=self._passcode,
-                           **self._v3_params)
+                       user_id=self._user_id,
+                       username=self._username,
+                       user_domain_id=u_domain_id,
+                       user_domain_name=u_domain_name,
+                       passcode=self._passcode,
+                       password=self._password,
+                       **self._v3_params)
